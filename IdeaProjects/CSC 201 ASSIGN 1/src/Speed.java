211f58c8d2fd2ee8dde19bb8de94dc2ce3db64ef
princeolass <princeolass@yahoo.comm>
2017-09-02 23:22:55 -0400

    Speed.java and test

--- IdeaProjects/CSC 201 ASSIGN 1/src/Speed.java
+++ IdeaProjects/CSC 201 ASSIGN 1/src/Speed.java
@@ -1,8 +1,15 @@
+/**GNU GENERAL PUBLIC LICENSE
+ Version 3, 29 June 2007
 
-
+ Copyright (C) 2014  Author Akeem Prince Olass.
+ Everyone is permitted to copy and distribute verbatim copies
+ of this license document, but changing it is not allowed.
+ *
+ */
 public class Speed {
-
+/** the class computes the speed of predetermined time and distance*/
 public  static float velocity  (float miles , float totTimehr)
+/** the velocity() function computes average speed km/hr */
     {
     final float One_Mileto_Km = (float) 1.6;
     float avgspeed;
@@ -13,13 +20,16 @@ public  static float velocity  (float miles , float totTimehr)
     } ;
 
 public static void main (String[] args ){
+    //variables are of type float
                 float miles = 24 ;
                 float hour = 1;
                 float min =  40;
                 float secs = 35;
                 float totTimehr = hour + ( min/60) + (secs / 3600);
-
-                velocity ( miles,totTimehr );
+    /**the velocity method calls the velocity function with argd
+     * of miles and time in hours only
+     */
+    velocity ( miles,totTimehr );
 
 
 };
